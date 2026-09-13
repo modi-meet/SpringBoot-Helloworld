852a68de22f31ebea43f4bc0fdfd24b090437fa8
meetintern-byte <meet.intern@teachmint.com>
2026-09-13 11:49:24 +0530

    add /bye endpoint

--- src/main/java/com/example/helloworldspringboot/greetController.java
+++ src/main/java/com/example/helloworldspringboot/greetController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class greetHelloController {
+public class greetController {
 
     @GetMapping ("/hello")
     public String hello() {
@@ -15,5 +15,10 @@ public class greetHelloController {
     @GetMapping ("/hi")
     public String hi() {
         return "<h1>Hi from server!</h1>";
+    }
+
+    @GetMapping ("/bye")
+    public String bye() {
+        return "<h1>Bye, see you later!</h1>";
     }
 }
